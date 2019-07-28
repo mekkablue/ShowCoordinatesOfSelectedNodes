5fe4b3168901082f0affd8b412194df85499cda8
Rainer Scheichelbauer <res@glyphsapp.com>
2019-07-28 10:51:24 +0200

    Add Spanish translation; turn off when selection has more than

--- ShowCoordinatesOfSelectedNodes.glyphsReporter/Contents/Resources/plugin.py
+++ ShowCoordinatesOfSelectedNodes.glyphsReporter/Contents/Resources/plugin.py
@@ -33,45 +33,44 @@ class ShowCoordinatesOfSelectedNodes(ReporterPlugin):
 			'de': u'Koordinaten ausgewählter Punkte',
 			'nl': u'coördinaten van geselecteerde punten',
 			'fr': u'les coordonnées des nœuds sélectionnés',
+			'es': u'las coordenadas de nodos seleccionados',
 		})
 		
 	def foreground(self, Layer):
 		currentSelection = Layer.selection
-		offset = 5.0 + self.getHandleSize() / self.getScale()
+		if len(currentSelection) < 21:
+			offset = 5.0 + self.getHandleSize() / self.getScale()
 		
-		if currentSelection:
-			# coordinates of on-curves
-			for thisItem in currentSelection:
-				if type(thisItem) is GSNode:
-					nodeType = thisItem.type
-					if nodeType == LINE or nodeType == CURVE:
-						xCoordinate = thisItem.x
-						yCoordinate = thisItem.y
-						self.drawTextAtPoint(
-							("%.1f, %.1f" % ( xCoordinate, yCoordinate )).replace(".0",""),
-							NSPoint( xCoordinate + offset, yCoordinate ),
-							fontColor=NSColor.brownColor()
-						)
+			if currentSelection:
+				# coordinates of on-curves
+				for thisItem in currentSelection:
+					if type(thisItem) is GSNode:
+						nodeType = thisItem.type
+						if nodeType == LINE or nodeType == CURVE:
+							xCoordinate = thisItem.x
+							yCoordinate = thisItem.y
+							self.drawTextAtPoint(
+								("%.1f, %.1f" % ( xCoordinate, yCoordinate )).replace(".0",""),
+								NSPoint( xCoordinate + offset, yCoordinate ),
+								fontColor=NSColor.brownColor()
+							)
 			
-			# length and angles of adjacent nodes
-			for thisPath in Layer.paths:
-				theseNodes = thisPath.nodes
-				thisNumberOfNodes = len( theseNodes )
-				for i in range( thisNumberOfNodes ):
-					previousNode = theseNodes[ (i-1) % thisNumberOfNodes ]
-					currentNode = theseNodes[ i ]
-					if (previousNode in currentSelection or currentNode in currentSelection) and not (previousNode.type == OFFCURVE and currentNode.type == OFFCURVE):
-						previousPoint = previousNode.position
-						currentPoint = currentNode.position
-						currentAngle = angle( previousPoint, currentPoint )
-						currentDistance = distance( previousPoint, currentPoint )
-						pointSum = addPoints( previousPoint, currentPoint )
-						pointInTheMiddle = NSPoint( pointSum.x * 0.5 + offset, pointSum.y * 0.5 )
-						self.drawTextAtPoint(
-							(u"%.1f @%.1f°" % ( currentDistance, currentAngle )).replace(".0",""),
-							pointInTheMiddle,
-							fontColor=NSColor.colorWithRed_green_blue_alpha_( 0.1, 0.7, 0.2, 1.0 )
-						)
-		
-
-	
+				# length and angles of adjacent nodes
+				for thisPath in Layer.paths:
+					theseNodes = thisPath.nodes
+					thisNumberOfNodes = len( theseNodes )
+					for i in range( thisNumberOfNodes ):
+						previousNode = theseNodes[ (i-1) % thisNumberOfNodes ]
+						currentNode = theseNodes[ i ]
+						if (previousNode in currentSelection or currentNode in currentSelection) and not (previousNode.type == OFFCURVE and currentNode.type == OFFCURVE):
+							previousPoint = previousNode.position
+							currentPoint = currentNode.position
+							currentAngle = angle( previousPoint, currentPoint )
+							currentDistance = distance( previousPoint, currentPoint )
+							pointSum = addPoints( previousPoint, currentPoint )
+							pointInTheMiddle = NSPoint( pointSum.x * 0.5 + offset, pointSum.y * 0.5 )
+							self.drawTextAtPoint(
+								(u"%.1f @%.1f°" % ( currentDistance, currentAngle )).replace(".0",""),
+								pointInTheMiddle,
+								fontColor=NSColor.colorWithRed_green_blue_alpha_( 0.1, 0.7, 0.2, 1.0 )
+							)
