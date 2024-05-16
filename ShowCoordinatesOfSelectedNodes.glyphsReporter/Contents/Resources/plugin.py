9c727687a773567c985fa0597b012a66611de2cf
schriftgestalt <georg.seifert@mac.com>
2024-05-16 22:00:48 +0200

    cleanup

--- ShowCoordinatesOfSelectedNodes.glyphsReporter/Contents/Resources/plugin.py
+++ ShowCoordinatesOfSelectedNodes.glyphsReporter/Contents/Resources/plugin.py
@@ -1,12 +1,14 @@
 # encoding: utf-8
 from __future__ import division, print_function, unicode_literals
 import objc
-from GlyphsApp import *
-from GlyphsApp.plugins import *
+from AppKit import NSColor, NSPoint
+from GlyphsApp import Glyphs, GSNode, LINE, CURVE, OFFCURVE, distance, addPoints
+from GlyphsApp.plugins import ReporterPlugin
 from math import degrees, atan2
 
+
 class ShowCoordinatesOfSelectedNodes(ReporterPlugin):
-	
+
 	@objc.python_method
 	def settings(self):
 		self.menuName = Glyphs.localize({
@@ -17,11 +19,11 @@ class ShowCoordinatesOfSelectedNodes(ReporterPlugin):
 			'es': 'las coordenadas de nodos seleccionados',
 			'pt': 'coordenadas dos nós selecionados',
 		})
-		Glyphs.registerDefault( "com.mekkablue.ShowCoordinatesOfSelectedNodes.showNodes", 1 )
-		Glyphs.registerDefault( "com.mekkablue.ShowCoordinatesOfSelectedNodes.showHandles", 1 )
-	
+		Glyphs.registerDefault("com.mekkablue.ShowCoordinatesOfSelectedNodes.showNodes", 1)
+		Glyphs.registerDefault("com.mekkablue.ShowCoordinatesOfSelectedNodes.showHandles", 1)
+
 	@objc.python_method
-	def angle( self, firstPoint, secondPoint ):
+	def angle(self, firstPoint, secondPoint):
 		"""
 		Returns the angle (in degrees) of the straight line between firstPoint and secondPoint,
 		0 degrees being the second point to the right of first point.
@@ -29,13 +31,13 @@ class ShowCoordinatesOfSelectedNodes(ReporterPlugin):
 		"""
 		xDiff = secondPoint.x - firstPoint.x
 		yDiff = secondPoint.y - firstPoint.y
-		return degrees(atan2(yDiff,xDiff))
-	
+		return degrees(atan2(yDiff, xDiff))
+
 	@objc.python_method
 	def foreground(self, layer):
 		showNodes = Glyphs.defaults["com.mekkablue.ShowCoordinatesOfSelectedNodes.showNodes"]
 		showHandles = Glyphs.defaults["com.mekkablue.ShowCoordinatesOfSelectedNodes.showHandles"]
-		
+
 		currentSelection = layer.selection
 		if len(currentSelection) < 13:
 			alpha = 0.7
@@ -47,9 +49,9 @@ class ShowCoordinatesOfSelectedNodes(ReporterPlugin):
 				# GLYPHS 2
 				green = NSColor.greenColor().colorWithAlphaComponent_(alpha)
 				brown = NSColor.brownColor().colorWithAlphaComponent_(alpha)
-			
+
 			offset = 5.0 + self.getHandleSize() / self.getScale()
-		
+
 			if currentSelection:
 
 				# coordinates of on-curves
@@ -62,31 +64,31 @@ class ShowCoordinatesOfSelectedNodes(ReporterPlugin):
 								yCoordinate = thisItem.y
 								self.drawTextAtPoint(
 									("%.1f, %.1f" % (xCoordinate, yCoordinate)).replace(".0", ""),
-									NSPoint( xCoordinate + offset, yCoordinate ),
+									NSPoint(xCoordinate + offset, yCoordinate),
 									fontColor=brown,
-									fontSize=10+2*Glyphs.handleSize,
+									fontSize=10 + 2 * Glyphs.handleSize,
 								)
-			
+
 				# length and angles of adjacent nodes
 				if showHandles:
 					for thisPath in layer.paths:
 						theseNodes = thisPath.nodes
-						thisNumberOfNodes = len( theseNodes )
-						for i in range( thisNumberOfNodes ):
-							previousNode = theseNodes[ (i-1) % thisNumberOfNodes ]
-							currentNode = theseNodes[ i ]
+						thisNumberOfNodes = len(theseNodes)
+						for i in range(thisNumberOfNodes):
+							previousNode = theseNodes[(i - 1) % thisNumberOfNodes]
+							currentNode = theseNodes[1]
 							if (previousNode in currentSelection or currentNode in currentSelection) and not (previousNode.type == OFFCURVE and currentNode.type == OFFCURVE):
 								previousPoint = previousNode.position
 								currentPoint = currentNode.position
-								currentAngle = self.angle( previousPoint, currentPoint )
-								currentDistance = distance( previousPoint, currentPoint )
-								pointSum = addPoints( previousPoint, currentPoint )
-								pointInTheMiddle = NSPoint( pointSum.x * 0.5 + offset, pointSum.y * 0.5 )
+								currentAngle = self.angle(previousPoint, currentPoint)
+								currentDistance = distance(previousPoint, currentPoint)
+								pointSum = addPoints(previousPoint, currentPoint)
+								pointInTheMiddle = NSPoint(pointSum.x * 0.5 + offset, pointSum.y * 0.5)
 								self.drawTextAtPoint(
 									("%.1f @%.1f°" % (currentDistance, currentAngle)).replace(".0", ""),
 									pointInTheMiddle,
 									fontColor=green,
-									fontSize=10+2*Glyphs.handleSize,
+									fontSize=10 + 2 * Glyphs.handleSize,
 								)
 
 	@objc.python_method
